402c0d117db734827e3b1f8a325c40190e044585
Bruno Meireles <brunomeireles23@gmail.com>
2023-01-10 17:24:36 -0300

    criação do projeto com typescript

---
+++ 01-fundamentos-reactjs-ts/src/App.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "http://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala Galera" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2022-05-03 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "http://github.com/brunomeirel3s.png",
+      name: "Bruno Meireles",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala Galera" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2023-01-07 14:00:00"),
+  },
+];
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
