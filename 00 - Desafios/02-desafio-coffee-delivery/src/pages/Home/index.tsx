d49b24ceeb06aeb3a3fed433a32ca762cef30345
Bruno Meireles <brunomeireles23@gmail.com>
2023-01-28 18:50:30 -0300

    adicionado card do café
    Desenvolvida a área no qual será listada as informações relacionadas aos cafés que serão vendidos

--- 00 - Desafios/02-desafio-coffee-delivery/src/pages/Home/index.tsx
+++ 00 - Desafios/02-desafio-coffee-delivery/src/pages/Home/index.tsx
@@ -1,10 +1,162 @@
 import { Intro } from "../../components/Intro";
-import { ContainerHome } from "./styles";
+import {
+  CardCoffee,
+  ContainerCoffeeList,
+  ContainerHome,
+  ContentCoffeeList,
+  PriceAmount,
+} from "./styles";
 
-export function Home(){
-    return(
-        <ContainerHome>
-            <Intro/>
-        </ContainerHome>
-    )
+import { ShoppingCartSimple } from "phosphor-react";
+
+import imgTradicional from "../../assets/coffees/black.svg";
+export function Home() {
+  return (
+    <ContainerHome>
+      <Intro />
+      <ContainerCoffeeList>
+        <h1>Nossos cafés</h1>
+        <ContentCoffeeList>
+          <CardCoffee>
+            <img src={imgTradicional} alt="Xícara de café tradicional" />
+            <span className="tipo">TRADICIONAL</span>
+            <h2>Expresso tradicional</h2>
+            <span className="info">
+              O tradicional café feito com água quente e grãos moídos
+            </span>
+            <PriceAmount>
+              <div className="price">
+                R$ <span>9,90</span>
+              </div>
+              <div className="amount">
+                <button>-</button>
+                <span>1</span>
+                <button>+</button>
+              </div>
+              <div className="cart">
+                <button>
+                  <ShoppingCartSimple size={22}/>
+                </button>            
+              </div>
+            </PriceAmount>
+          </CardCoffee>
+          <CardCoffee>
+            <img src={imgTradicional} alt="Xícara de café tradicional" />
+            <span className="tipo">TRADICIONAL</span>
+            <h2>Expresso tradicional</h2>
+            <span className="info">
+              O tradicional café feito com água quente e grãos moídos
+            </span>
+            <PriceAmount>
+              <div className="price">
+                R$ <span>9,90</span>
+              </div>
+              <div className="amount">
+                <button>-</button>
+                <span>1</span>
+                <button>+</button>
+              </div>
+              <div className="cart">
+                <button>
+                  <ShoppingCartSimple size={22}/>
+                </button>            
+              </div>
+            </PriceAmount>
+          </CardCoffee>
+          <CardCoffee>
+            <img src={imgTradicional} alt="Xícara de café tradicional" />
+            <span className="tipo">TRADICIONAL</span>
+            <h2>Expresso tradicional</h2>
+            <span className="info">
+              O tradicional café feito com água quente e grãos moídos
+            </span>
+            <PriceAmount>
+              <div className="price">
+                R$ <span>9,90</span>
+              </div>
+              <div className="amount">
+                <button>-</button>
+                <span>1</span>
+                <button>+</button>
+              </div>
+              <div className="cart">
+                <button>
+                  <ShoppingCartSimple size={22}/>
+                </button>            
+              </div>
+            </PriceAmount>
+          </CardCoffee>
+          <CardCoffee>
+            <img src={imgTradicional} alt="Xícara de café tradicional" />
+            <span className="tipo">TRADICIONAL</span>
+            <h2>Expresso tradicional</h2>
+            <span className="info">
+              O tradicional café feito com água quente e grãos moídos
+            </span>
+            <PriceAmount>
+              <div className="price">
+                R$ <span>9,90</span>
+              </div>
+              <div className="amount">
+                <button>-</button>
+                <span>1</span>
+                <button>+</button>
+              </div>
+              <div className="cart">
+                <button>
+                  <ShoppingCartSimple size={22}/>
+                </button>            
+              </div>
+            </PriceAmount>
+          </CardCoffee>
+          <CardCoffee>
+            <img src={imgTradicional} alt="Xícara de café tradicional" />
+            <span className="tipo">TRADICIONAL</span>
+            <h2>Expresso tradicional</h2>
+            <span className="info">
+              O tradicional café feito com água quente e grãos moídos
+            </span>
+            <PriceAmount>
+              <div className="price">
+                R$ <span>9,90</span>
+              </div>
+              <div className="amount">
+                <button>-</button>
+                <span>1</span>
+                <button>+</button>
+              </div>
+              <div className="cart">
+                <button>
+                  <ShoppingCartSimple size={22}/>
+                </button>            
+              </div>
+            </PriceAmount>
+          </CardCoffee>
+          <CardCoffee>
+            <img src={imgTradicional} alt="Xícara de café tradicional" />
+            <span className="tipo">TRADICIONAL</span>
+            <h2>Expresso tradicional</h2>
+            <span className="info">
+              O tradicional café feito com água quente e grãos moídos
+            </span>
+            <PriceAmount>
+              <div className="price">
+                R$ <span>9,90</span>
+              </div>
+              <div className="amount">
+                <button>-</button>
+                <span>1</span>
+                <button>+</button>
+              </div>
+              <div className="cart">
+                <button>
+                  <ShoppingCartSimple size={22}/>
+                </button>            
+              </div>
+            </PriceAmount>
+          </CardCoffee>
+        </ContentCoffeeList>
+      </ContainerCoffeeList>
+    </ContainerHome>
+  );
 }
--- 00 - Desafios/02-desafio-coffee-delivery/src/pages/Home/styles.ts
+++ 00 - Desafios/02-desafio-coffee-delivery/src/pages/Home/styles.ts
@@ -1,5 +1,125 @@
 import styled from "styled-components";
 
-export const ContainerHome = styled.main`
-  
+export const ContainerHome = styled.main``;
+
+export const ContainerCoffeeList = styled.div`
+  h1,
+  h2,
+  h3 {
+    color: ${(props) => props.theme["base-subtitle"]};
+    font-family: "Baloo 2";
+  }
+`;
+
+export const ContentCoffeeList = styled.div`
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export const CardCoffee = styled.div`
+  max-width: 16rem;
+  background: ${(props) => props.theme["base-card"]};
+  border-radius: 6px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 1rem;
+  text-align: center;
+
+  img {
+    margin-top: -2rem;
+  }
+
+  h2 {
+    margin: 0.5rem 0;
+  }
+
+  .tipo {
+    color: ${(props) => props.theme["yellow-dark"]};
+    background: ${(props) => props.theme["yellow-light"]};
+    padding: 0.5rem;
+    border-radius: 15px;
+    margin-top: 1rem;
+    font-weight: bold;
+    font-size: 0.8rem;
+  }
+
+  .info {
+    color: ${(props) => props.theme["base-label"]};
+  }
+`;
+
+export const PriceAmount = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  align-items: center;
+  font-size: 1.2rem;
+  margin-top: 2rem;
+
+  .price {
+    color: ${(props) => props.theme["base-text"]};
+
+    span {
+      font-weight: bold;
+      font-size: 1.6rem;
+      font-family: "Baloo 2";
+    }
+  }
+
+  .amount {
+    background: ${(props) => props.theme["base-button"]};
+    padding: 0.5rem;
+    border-radius: 6px;
+
+    display: flex;
+    flex-direction: row;
+    gap: 0.5rem;
+    align-items: center;
+    justify-content: center;
+
+    button {
+      background: transparent;
+      border: none;
+      outline: none;
+      font-size: inherit;
+      color: ${(props) => props.theme["purple-dark"]};
+
+      transition: scale 0.09s;
+
+      &:hover {
+        cursor: pointer;
+      }
+
+      &:active {
+        scale: 0.9;
+      }
+    }
+  }
+
+  .cart {
+    button {
+      padding: 0.5rem;
+      background: ${(props) => props.theme["purple-dark"]};
+      border: none;
+      color: ${(props) => props.theme["white"]};
+      border-radius: 6px;
+
+      transition: background-color 0.2s;
+
+      &:hover {
+        cursor: pointer;
+        background: ${(props) => props.theme["purple"]};
+      }
+
+      &:active {
+        scale: 0.95;
+      }
+    }
+  }
 `;
