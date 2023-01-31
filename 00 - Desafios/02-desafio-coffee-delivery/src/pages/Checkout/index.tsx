9827ef56405fd9ff5274df06206c9b89b71f0324
Bruno Meireles <brunomeireles23@gmail.com>
2023-01-31 15:45:28 -0300

    31-01-2022

--- 00 - Desafios/02-desafio-coffee-delivery/src/pages/Checkout/index.tsx
+++ 00 - Desafios/02-desafio-coffee-delivery/src/pages/Checkout/index.tsx
@@ -1,10 +1,12 @@
-import { MapPin } from "phosphor-react";
+import { CurrencyDollar, MapPin } from "phosphor-react";
+import { ButtonFormaPagamento } from "./components/ButtonFormaPagamento";
 import {
   ContainerCafeSelecionado,
   ContainerCheckout,
   ContainerCompletePedido,
   ContainerEndereco,
   ContainerFormEndereco,
+  ContainerPagamento,
 } from "./styles";
 
 export function Checkout() {
@@ -13,14 +15,14 @@ export function Checkout() {
       <ContainerCompletePedido>
         <h4>Complete seu pedido</h4>
         <ContainerEndereco>
-          <div className="enderecoEntrega">
-            <div className="icone">
+          <div className="headerForm">
+            <div className="iconYellow">
               <MapPin size={25} />
             </div>
             <div>
-              <span className="spanEndereco">Endereço de Entrega</span>
+              <span className="titleForm">Endereço de Entrega</span>
               <br />
-              <span className="spanInforme">
+              <span className="subtitleForm">
                 Informe o endereço onde deseja receber seu pedido
               </span>
             </div>
@@ -37,9 +39,44 @@ export function Checkout() {
                 placeholder="Rua"
                 style={{ maxWidth: "100%" }}
               />
+
+              <div className="containerTwoInputs">
+                <input type="text" placeholder="Número" />
+                <input type="text" placeholder="Complemento" />
+              </div>
+              <div className="containerThreeInputs">
+                <input type="text" placeholder="Bairro" />
+                <input type="text" placeholder="Cidade" />
+                <input type="text" placeholder="UF" maxLength={2} />
+              </div>
             </ContainerFormEndereco>
           </form>
         </ContainerEndereco>
+        <ContainerPagamento>
+          <div className="headerForm">
+            <div className="iconPurple">
+              <CurrencyDollar size={25} />
+            </div>
+            <div>
+              <span className="titleForm">Pagamento</span>
+              <br />
+              <span className="subtitleForm">
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </span>
+            </div>
+          </div>
+          <div className="containerOpcoesPagamento">
+            <ButtonFormaPagamento
+              formaPagamento="credito"
+              label="CARTÃO DE CRÉDITO"
+            />
+            <ButtonFormaPagamento
+              formaPagamento="debito"
+              label="CARTÃO DE DÉBITO"
+            />
+            <ButtonFormaPagamento formaPagamento="dinheiro" label="DINHEIRO" />
+          </div>
+        </ContainerPagamento>
       </ContainerCompletePedido>
       <ContainerCafeSelecionado></ContainerCafeSelecionado>
     </ContainerCheckout>
--- 00 - Desafios/02-desafio-coffee-delivery/src/pages/Checkout/styles.ts
+++ 00 - Desafios/02-desafio-coffee-delivery/src/pages/Checkout/styles.ts
@@ -20,31 +20,35 @@ export const ContainerCompletePedido = styled.div`
     color: ${(props) => props.theme["base-subtitle"]};
     font-family: "Baloo 2";
   }
+
+  .headerForm {
+    display: flex;
+    flex-direction: row;
+    gap: 0.5rem;
+  }
+
+  .iconYellow {
+    color: ${(props) => props.theme["yellow-dark"]};
+  }
+
+  .iconPurple {
+    color: ${(props) => props.theme["purple-dark"]};
+  }
+
+  .titleForm {
+    color: ${(props) => props.theme["base-subtitle"]};
+    font-size: 1.2rem;
+  }
+
+  .subtitleForm {
+    color: ${(props) => props.theme["base-text"]};
+  }
 `;
 
 export const ContainerEndereco = styled.div`
   padding: 2rem;
   background: ${(props) => props.theme["base-card"]};
   border-radius: 6px;
-
-  .enderecoEntrega {
-    display: flex;
-    flex-direction: row;
-    gap: 0.5rem;
-  }
-
-  .icone {
-    color: ${(props) => props.theme["yellow-dark"]};
-  }
-
-  .spanEndereco {
-    color: ${(props) => props.theme["base-subtitle"]};
-    font-size: 1.2rem;
-  }
-
-  .spanInforme {
-    color: ${(props) => props.theme["base-text"]};
-  }
 `;
 
 export const ContainerFormEndereco = styled.div`
@@ -59,11 +63,33 @@ export const ContainerFormEndereco = styled.div`
     border-radius: 6px;
     border: 1px solid ${(props) => props.theme["base-button"]};
     background: ${(props) => props.theme["base-input"]};
+    color: ${(props) => props.theme["base-text"]};
+  }
+
+  .containerTwoInputs {
+    display: grid;
+    grid-template-columns: 12.5rem 1fr;
+    gap: 1rem;
+  }
+
+  .containerThreeInputs {
+    display: grid;
+    grid-template-columns: 12.5rem 1fr 4rem;
+    gap: 1rem;
   }
 `;
 
 export const ContainerPagamento = styled.div`
-  padding: 1rem;
+  padding: 2rem;
+  background: ${(props) => props.theme["base-card"]};
+  border-radius: 6px;
+
+  .containerOpcoesPagamento {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 1rem;
+    margin-top: 2rem;
+  }
 `;
 
 export const ContainerCafeSelecionado = styled.div`
