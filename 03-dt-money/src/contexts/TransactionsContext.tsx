025c3e9d2a21b6a8abde9d50777b7b98124d8fc6
Bruno Meireles <brunomeireles23@gmail.com>
2023-03-18 20:26:48 -0300

    Ajustes no: context, react hook forms, modal
    Realizados ajustes no contexto, adicionado react hook form e ajustado a função de buscas por transações

--- 03-dt-money/src/contexts/TransactionsContext.tsx
+++ 03-dt-money/src/contexts/TransactionsContext.tsx
@@ -11,6 +11,7 @@ interface Transaction {
 
 interface TransactionContextType {
   transactions: Transaction[];
+  fetchTransactions: (query?: string) => Promise<void>;
 }
 export const TransactionsContext = createContext({} as TransactionContextType);
 
@@ -19,18 +20,23 @@ interface TransactionProviderProps {
 }
 export function TransactionsProvider({ children }: TransactionProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
-  async function loadTransactions() {
-    const response = await fetch("http://localhost:3333/transactions");
+
+  async function fetchTransactions(query?: string) {
+    const url = new URL("http://localhost:3333/transactions");
+    if (query) {
+      url.searchParams.append("q", query);
+    }
+    const response = await fetch(url);
     const data = await response.json();
 
     setTransactions(data);
   }
 
   useEffect(() => {
-    loadTransactions();
+    fetchTransactions();
   }, []);
   return (
-    <TransactionsContext.Provider value={{ transactions }}>
+    <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
       {children}
     </TransactionsContext.Provider>
   );
