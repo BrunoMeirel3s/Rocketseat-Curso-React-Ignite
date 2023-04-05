7c57f5dbcebcbf4512337f55939c8ba38e62f556
Bruno Meireles <brunomeireles23@gmail.com>
2023-04-05 06:00:33 -0300

    Criação do carrosel

--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -1,26 +1,51 @@
-import { styled } from "../styles";
+import { useKeenSlider } from "keen-slider/react";
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
 
-const Button = styled("button", {
-  backgroundColor: "$green300",
-  borderRadius: 8,
-  border: 0,
-  padding: "4px 8px",
+import camiseta1 from "../asssets/camisetas/1.png";
+import camiseta2 from "../asssets/camisetas/2.png";
+import camiseta3 from "../asssets/camisetas/3.png";
 
-  span: {
-    fontWeight: "bold",
-  },
-  "&:hover": {
-    filter: "brightness(0.8)",
-  },
-});
+import "keen-slider/keen-slider.min.css";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <>
-      <Button>
-        {" "}
-        <span>Teste</span> Enviar
-      </Button>
-    </>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={camiseta1} width={520} height={480} alt="" />
+        <footer>
+          <strong>CAmiseta X</strong>
+          <span>R$ 79.90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta2} width={520} height={480} alt="" />
+        <footer>
+          <strong>CAmiseta Y</strong>
+          <span>R$ 79.90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+        <footer>
+          <strong>CAmiseta Y</strong>
+          <span>R$ 79.90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+        <footer>
+          <strong>CAmiseta Y</strong>
+          <span>R$ 79.90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
