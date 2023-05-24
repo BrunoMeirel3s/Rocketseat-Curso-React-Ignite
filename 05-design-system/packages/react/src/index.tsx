fde2e9a328f3ce86217e813bff0e9eebcdfd060d
Bruno Meireles <brunomeireles23@gmail.com>
2023-05-24 06:04:09 -0300

    criado primeiro components no storybook

--- 05-design-system/packages/react/src/index.tsx
+++ 05-design-system/packages/react/src/index.tsx
@@ -1,12 +1,31 @@
 import { styled } from "./styles";
+import { ComponentProps } from "react";
 
-const Buttton = styled("button", {
+export const Button = styled("button", {
   fontFamily: "$default",
   backgroundColor: "$ignite500",
-  borderRadius: "$md",
+  borderRadius: "$sm",
   padding: "$4",
+  border: 0,
+  fontWeight: "bold",
+  color: "$white",
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: "$2 $4",
+      },
+      big: {
+        fontSize: 16,
+        padding: "$3 $6",
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: "small",
+  },
 });
 
-export function App() {
-  return <Buttton>Hello World</Buttton>;
-}
+export type ButtonProps = ComponentProps<typeof Button>;
