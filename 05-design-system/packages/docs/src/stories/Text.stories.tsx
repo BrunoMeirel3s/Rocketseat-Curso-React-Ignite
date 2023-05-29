6e38ee24d007e35e6afb4cffe22727bfb7536279
Bruno Meireles <brunomeireles23@gmail.com>
2023-05-29 05:59:43 -0300

    criação de novos stories e configuração do turborepo

---
+++ 05-design-system/packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,22 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, voluptates officiis nesciunt tempora nulla delectus consectetur inventore optio similique vitae? Voluptates autem culpa quidem minus hic nemo corrupti tempora impedit!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {
+  args: {},
+}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
--- 05-design-system/packages/react/src/index.tsx
+++ 05-design-system/packages/react/src/index.tsx
@@ -1,31 +1,3 @@
-import { styled } from "./styles";
-import { ComponentProps } from "react";
+export * from './components/Box'
 
-export const Button = styled("button", {
-  fontFamily: "$default",
-  backgroundColor: "$ignite500",
-  borderRadius: "$sm",
-  padding: "$4",
-  border: 0,
-  fontWeight: "bold",
-  color: "$white",
-
-  variants: {
-    size: {
-      small: {
-        fontSize: 14,
-        padding: "$2 $4",
-      },
-      big: {
-        fontSize: 16,
-        padding: "$3 $6",
-      },
-    },
-  },
-
-  defaultVariants: {
-    size: "small",
-  },
-});
-
-export type ButtonProps = ComponentProps<typeof Button>;
+export * from './components/Text'
