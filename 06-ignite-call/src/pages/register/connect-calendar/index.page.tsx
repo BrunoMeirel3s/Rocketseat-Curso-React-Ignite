9f887797a1cf01a286e2d9ff98a418ddfdce11e7
Bruno Meireles <brunomeireles23@gmail.com>
2023-07-03 05:52:48 -0300

    added: autenticação utilizando nextauth

--- 06-ignite-call/src/pages/register/connect-calendar/index.page.tsx
+++ 06-ignite-call/src/pages/register/connect-calendar/index.page.tsx
@@ -2,9 +2,11 @@ import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
 import { Container, Header } from '../styles'
 import { ArrowRight } from 'phosphor-react'
 import { ConnectBox, ConnectItem } from './styles'
+import { signIn, useSession } from 'next-auth/react'
 
 export default function Register() {
   // async function handleRegister(data: RegisterFormData) {}
+  const session = useSession()
 
   return (
     <Container>
@@ -20,11 +22,16 @@ export default function Register() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant="secondary" size={'sm'}>
+          <Button
+            variant="secondary"
+            size={'sm'}
+            onClick={() => signIn('google')}
+          >
             Conectar
             <ArrowRight />
           </Button>
         </ConnectItem>
+        <Text>{JSON.stringify(session)}</Text>
         <Button type="submit">
           Proximo passo
           <ArrowRight />
