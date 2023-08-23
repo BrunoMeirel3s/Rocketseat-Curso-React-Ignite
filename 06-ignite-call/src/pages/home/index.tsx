2f08f179fedd74f12f88db6dad1f4805f121cabc
Bruno Meireles <brunomeireles23@gmail.com>
2023-08-23 06:03:50 -0300

    added: ajustes para SEO

--- 06-ignite-call/src/pages/home/index.tsx
+++ 06-ignite-call/src/pages/home/index.tsx
@@ -4,27 +4,34 @@ import { Container, Hero, Preview } from './styles'
 import previewImage from '../../assets/app-preview.png'
 import Image from 'next/image'
 import { ClaimUsernamForm } from './components/ClaimUsernamForm'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading size="4xl">Agendamento descomplicado</Heading>
-        <Text size="lg">
-          Conecte seu calendário e permita que as pessoas marquem agendamentos
-          no seu tempo livre
-        </Text>
-        <ClaimUsernamForm />
-      </Hero>
-      <Preview>
-        <Image
-          src={previewImage}
-          alt="Calendário simbolizando a aplicação em funcionamento"
-          height={400}
-          quality={100}
-          priority
-        />
-      </Preview>
-    </Container>
+    <>
+      <NextSeo
+        title="Descomplique sua agenda | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre"
+      />
+      <Container>
+        <Hero>
+          <Heading size="4xl">Agendamento descomplicado</Heading>
+          <Text size="lg">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre
+          </Text>
+          <ClaimUsernamForm />
+        </Hero>
+        <Preview>
+          <Image
+            src={previewImage}
+            alt="Calendário simbolizando a aplicação em funcionamento"
+            height={400}
+            quality={100}
+            priority
+          />
+        </Preview>
+      </Container>
+    </>
   )
 }
--- 06-ignite-call/src/pages/register/index.page.tsx
+++ 06-ignite-call/src/pages/register/index.page.tsx
@@ -8,6 +8,7 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { api } from '@/lib/axios'
 import { AxiosError } from 'axios'
+import { NextSeo } from 'next-seo'
 
 const registerFormSchema = z.object({
   username: z
@@ -60,39 +61,45 @@ export default function Register() {
   }
 
   return (
-    <Container>
-      <Header>
-        <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
-        <Text>
-          Precisamos de algumas informações para criar seu perfil!, Ah, você
-          pode editar essas informações depois
-        </Text>
+    <>
+      <NextSeo title="Crie uma conta | Ignite Call" />
 
-        <MultiStep size={4} currentStep={1} />
-      </Header>
+      <Container>
+        <Header>
+          <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
+          <Text>
+            Precisamos de algumas informações para criar seu perfil!, Ah, você
+            pode editar essas informações depois
+          </Text>
 
-      <Form as="form" onSubmit={handleSubmit(handleRegister)}>
-        <label>
-          <Text size="sm">Nome de usuário</Text>
-          <TextInput
-            prefix="ignite.com/"
-            placeholder="seu-usuário"
-            {...register('username')}
-          />
-          {errors.username && <FormError>{errors.username.message}</FormError>}
-        </label>
+          <MultiStep size={4} currentStep={1} />
+        </Header>
 
-        <label>
-          <Text size="sm">Nome completo</Text>
-          <TextInput placeholder="Seu nome" {...register('name')} />
-          {errors.name && <FormError>{errors.name.message}</FormError>}
-        </label>
+        <Form as="form" onSubmit={handleSubmit(handleRegister)}>
+          <label>
+            <Text size="sm">Nome de usuário</Text>
+            <TextInput
+              prefix="ignite.com/"
+              placeholder="seu-usuário"
+              {...register('username')}
+            />
+            {errors.username && (
+              <FormError>{errors.username.message}</FormError>
+            )}
+          </label>
 
-        <Button type="submit" disabled={isSubmitting}>
-          Próximo passo
-          <ArrowRight />
-        </Button>
-      </Form>
-    </Container>
+          <label>
+            <Text size="sm">Nome completo</Text>
+            <TextInput placeholder="Seu nome" {...register('name')} />
+            {errors.name && <FormError>{errors.name.message}</FormError>}
+          </label>
+
+          <Button type="submit" disabled={isSubmitting}>
+            Próximo passo
+            <ArrowRight />
+          </Button>
+        </Form>
+      </Container>
+    </>
   )
 }
